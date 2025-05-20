5da692163376190ad505e5e188d49c68ea8a023c
NoortjeKlaassen <noortje06@kpnmail.nl>
2025-05-20 12:04:18 +0200

    Minigame + beginpagina knop href

---
+++ minigame.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Bananen challenge
+    const donkergroeneBanaan = document.querySelector('img[src="img/Minigame/Donkergroene-banaanCB.png"]');
+
+    donkergroeneBanaan.addEventListener('click', () => {
+        donkergroeneBanaan.src = 'img/Minigame/Donkergroene-banaanUIT.png';
+    });
+
+    const overrijpeBanaan = document.querySelector('img[src="img/Minigame/Overrijpe-banaanCB.png"]');
+
+    overrijpeBanaan.addEventListener('click', () => {
+        overrijpeBanaan.src = 'img/Minigame/Overrijpe-banaanUIT.png';
+    });
+
+    const groeneBanaan = document.querySelector('img[src="img/Minigame/Groene-banaanCB.png"]');
+
+    groeneBanaan.addEventListener('click', () => {
+        groeneBanaan.src = 'img/Minigame/Groene-banaanUIT.png';
+    });
+
+    const rijpeBanaan = document.querySelector('img[src="img/Minigame/Rijpe-banaanCB.png"]');
+
+    rijpeBanaan.addEventListener('click', () => {
+        rijpeBanaan.src = 'img/Minigame/Rijpe-banaanUIT.png';
+    });
+
+    // Appel challenge
+    const geleAppel = document.querySelector('img[src="img/Minigame/Gele-appelCB.png"]');
+
+    geleAppel.addEventListener('click', () => {
+        geleAppel.src = 'img/Minigame/Gele-appelUIT.png';
+    });
+
+    const rozeAppel = document.querySelector('img[src="img/Minigame/Roze-appelCB.png"]');
+
+    rozeAppel.addEventListener('click', () => {
+        rozeAppel.src = 'img/Minigame/Roze-appelUIT.png';
+    });
+
+    const rodeAppel = document.querySelector('img[src="img/Minigame/Rode-appelCB.png"]');
+
+    rodeAppel.addEventListener('click', () => {
+        rodeAppel.src = 'img/Minigame/Rode-appelUIT.png';
+    });
+
+    const groeneAppel = document.querySelector('img[src="img/Minigame/Groene-appelCB.png"]');
+
+    groeneAppel.addEventListener('click', () => {
+        groeneAppel.src = 'img/Minigame/Groene-appelUIT.png';
+    });
+
+    // Paprika challenge
+    const oranjePaprika = document.querySelector('img[src="img/Minigame/Oranje-paprikaCB.png"]');
+
+    oranjePaprika.addEventListener('click', () => {
+        oranjePaprika.src = 'img/Minigame/Oranje-paprikaUIT.png';
+    });
+
+    const groenePaprika = document.querySelector('img[src="img/Minigame/Groene-paprikaCB.png"]');
+
+    groenePaprika.addEventListener('click', () => {
+        groenePaprika.src = 'img/Minigame/Groene-paprikaUIT.png';
+    });
+
+    const rodePaprika = document.querySelector('img[src="img/Minigame/Rode-paprikaCB.png"]');
+
+    rodePaprika.addEventListener('click', () => {
+        rodePaprika.src = 'img/Minigame/Rode-paprikaUIT.png';
+    });
+
+    const gelePaprika = document.querySelector('img[src="img/Minigame/Gele-paprikaCB.png"]');
+
+    gelePaprika.addEventListener('click', () => {
+        gelePaprika.src = 'img/Minigame/Gele-paprikaUIT.png';
+    });
+
+    // Chips challenge
+    const groeneChips = document.querySelector('img[src="img/Minigame/Groene-chipsCB.png"]');
+
+    groeneChips.addEventListener('click', () => {
+        groeneChips.src = 'img/Minigame/Groene-chipsUIT.png';
+    });
+
+    const geleChips = document.querySelector('img[src="img/Minigame/Gele-chipsCB.png"]');
+
+    geleChips.addEventListener('click', () => {
+        geleChips.src = 'img/Minigame/Gele-chipsUIT.png';
+    });
+
+    const rodeChips = document.querySelector('img[src="img/Minigame/Rode-chipsCB.png"]');
+
+    rodeChips.addEventListener('click', () => {
+        rodeChips.src = 'img/Minigame/Rode-chipsUIT.png';
+    });
+
+    const blauweChips = document.querySelector('img[src="img/Minigame/Blauwe-chipsCB.png"]');
+
+    blauweChips.addEventListener('click', () => {
+        blauweChips.src = 'img/Minigame/Blauwe-chipsUIT.png';
+    });
+
+});
